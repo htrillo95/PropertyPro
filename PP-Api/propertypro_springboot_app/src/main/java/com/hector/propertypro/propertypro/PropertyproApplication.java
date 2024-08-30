bb7c69b13984c93e6e9d51b4b2bb287881d4e380
Hector Trillo <htrillo95@gmail.com>
2024-08-30 11:31:31 -0400

    first commit

---
+++ PP-Api/propertypro_springboot_app/src/main/java/com/hector/propertypro/propertypro/PropertyproApplication.java
@@ -0,0 +1,13 @@
+package com.hector.propertypro.propertypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyproApplication.class, args);
+	}
+
+}
