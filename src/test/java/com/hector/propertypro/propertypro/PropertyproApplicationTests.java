3a6ce54f960e01b41050846aa2313d55e944f261
Hector Trillo <htrillo95@gmail.com>
2024-08-30 11:37:28 -0400

    Initial commit

---
+++ src/test/java/com/hector/propertypro/propertypro/PropertyproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hector.propertypro.propertypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
